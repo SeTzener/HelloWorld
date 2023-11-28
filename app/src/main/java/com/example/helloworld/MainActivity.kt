5cb0e0451984291bdf0c73e5aad07a89fa4ba2ad
Gaetano Zito <gv.zito@hotmail.com>
2023-11-28 16:42:05 +0100

    https://www.youtube.com/watch?v=gg-KBGH9T8s&list=PLSrm9z4zp4mEWwyiuYgVMWcDFdsebhM-r&index=28
    Bottom Navigation kickstart

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.helloworld.bottom.navigation.example.MainScreen
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,7 +54,8 @@ class MainActivity : ComponentActivity() {
 //                    NavigationControllerExample()
 //                    SendArgumentsBetweenScreenExample()
 //                    NestedNavigationPart1Example()
-                    NestedNavigationPart2Example()
+//                    NestedNavigationPart2Example()
+                    MainScreen()
                 }
             }
         }
