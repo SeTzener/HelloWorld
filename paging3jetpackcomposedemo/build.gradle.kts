96c74c79c2fba4ab7f12c8e0d4e219c3be2d4b24
TanoAirthings <gaetano.zito@airthings.com>
2023-12-04 11:31:10 +0100

    Setting up for the new project

--- paging3jetpackcomposedemo/build.gradle.kts
+++ paging3jetpackcomposedemo/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.helloworld"
+    namespace = "com.example.paging3jetpackcomposedemo"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.helloworld"
-        minSdk = 31
+        applicationId = "com.example.paging3jetpackcomposedemo"
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -52,21 +52,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-
-    implementation("io.coil-kt:coil-compose:2.1.0")
-    implementation("com.google.dagger:hilt-android:2.44")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("androidx.compose.material:material:1.5.4")
-
     implementation("androidx.compose.material3:material3-android:1.2.0-alpha12")
-    implementation("androidx.wear.compose:compose-material3:1.0.0-alpha14")
 
     // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
@@ -83,11 +76,10 @@ dependencies {
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.33.2-alpha")
     implementation("androidx.activity:activity-compose:1.8.0-alpha07")
 
-    implementation("javax.inject:javax.inject:1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
